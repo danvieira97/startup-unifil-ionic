e5c398d2125ed0f76a02f276c83d6e11435537f0
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-11-28 17:30:25 -0300

    implementação na página de registro de restaurante

--- src/app/register-restaurants/register-restaurants.page.ts
+++ src/app/register-restaurants/register-restaurants.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register-restaurants',
@@ -8,34 +9,60 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./register-restaurants.page.scss'],
 })
 export class RegisterRestaurantsPage implements OnInit {
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient, private alertController: AlertController) {}
 
   public formRegister: FormGroup;
   ngOnInit() {
     this.formRegister = new FormGroup({
       name: new FormControl(),
       email: new FormControl(),
+      availableTables: new FormControl(),
       password: new FormControl(),
       confirmPassword: new FormControl(),
     });
   }
 
   async onSubmit() {
-    console.log(this.formRegister.value);
     const name = this.formRegister.value.name;
     const email = this.formRegister.value.email;
+    const availableTables = this.formRegister.value.availableTables;
     const password = this.formRegister.value.password;
     const confirmPassword = this.formRegister.value.confirmPassword;
+
+    const availableDays = []
+    for (let i = 1; i <= 31; i++) {
+      availableDays.push(i);
+    }
 
     const restaurant = {
       name: name,
       email: email,
+      availableTables: availableTables,
+      availableDays: availableDays,
       password: password,
       confirmPassword: confirmPassword,
     };
 
+    const alert = await this.alertController.create({
+      header: name,
+      cssClass: 'custom-alert',
+      message: 'Registrado com sucesso',
+      buttons: ['OK'],
+    });
+
+    const alertError = await this.alertController.create({
+      header: "Erro",
+      cssClass: 'custom-alert',
+      message: 'Erro ao registrar o restaurante',
+      buttons: ['OK']
+    });
+
     await this.http
       .post('http://localhost:3000/restaurants/createRestaurant', restaurant)
-      .subscribe();
+      .subscribe({
+        next: (res) => {if(res == "OK"){alert.present();}},
+        error: error => {alertError.present();},
+        complete: () => alert.present()
+      })
   }
 }
